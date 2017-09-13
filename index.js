edb87eea303175f1388bf0771fee501e27bee954
Benjamin Poulinet <benjamin.poulinet@zeniho.com>
2017-09-13 09:23:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,19 +21,19 @@ app.set('views', path.join(__dirname, 'views'))
 app.get('/', (request, response) => {
   // Hello, it's good to see you here.
   // Change the name and email so we know who you are!
-  // let args = {
-  //     data: {
-  //       "firstname":"Benjamin",
-  //       "lastname":"Poulinet",
-  //       "email":"bpoulinet@gmail.com",
-  //       "id":appID
-  //     },
-  //     headers: { "Content-Type": "application/json" }
-  // };
+   let args = {
+       data: {
+         "firstname":"Benjamin",
+         "lastname":"Poulinet",
+         "email":"bpoulinet@gmail.com",
+         "id":appID
+       },
+       headers: { "Content-Type": "application/json" }
+  };
   // You are nearly there!
-  // client.post("http://onboarder.cleverapps.io/play", args, function (data, response) {
-  //   console.log(response);
-  // });
+  client.post("http://onboarder.cleverapps.io/play", args, function (data, response) {
+     console.log(response);
+  });
   // If you want to dig further you should watch this: https://www.youtube.com/watch?v=CeaoTAXkIZE
 
   response.render('home', {
